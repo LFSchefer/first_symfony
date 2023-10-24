2f5807928675a78b3fb789a68c3b20fe211e5f10
louis frederic schefer <lf.schefer@gmail.com>
2023-10-24 17:32:55 +0200

    template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -22,6 +22,26 @@ class LuckyController extends AbstractController
     }
 
 
+
+    #[Route('/')]
+    public function homepage(): Response
+    {
+      $tracks = [
+        ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+        ['song' => 'Waterfalls', 'artist' => 'TLC'],
+        ['song' => 'Creep', 'artist' => 'Radiohead'],
+        ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+        ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+        ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+      ];
+
+      return $this->render('vinyl/homepage.html.twig', [
+        'title' => 'PB & James',
+        'tracks' => $tracks
+      ]);
+    }
+
+
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
