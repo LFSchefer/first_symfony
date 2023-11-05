d90e348839a2cac8440bfe611a9b36563ba00f1c
louis frederic schefer <lf.schefer@gmail.com>
2023-11-05 02:23:05 +0100

    log service

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -6,18 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 
 
 class SongController extends AbstractController
 {
     #[Route('api/song/{id<\d+>}', name: 'get_song' ,methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
       $song = [
         'id' => $id,
         'name' => 'Waterfalls',
         'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
       ];
+
+      $logger->info('Returning API response for song {song}', [
+        'song' => $id,
+      ]);
 
       return new JsonResponse($song);
     }
